4638862d849adc1f5c46b36d806466086a630f6b
beemanbp03 <boulder.payne.beeman@gmail.com>
2023-09-28 12:51:51 -0500

    started work on scraping the Space Marine page of wawahpedia, including the faction title, faction description, and oath of moment.

--- scraper.js
+++ scraper.js
@@ -4,6 +4,9 @@ const cheerio = require('cheerio');
 const axios = require('axios').default;
 const app = express();
 
+//Set any JSON in a response to be formatted
+app.set('json spaces', 2);
+
 app.get("/", (req, res) => {
     res.send(`
         <h1>table-top-champion Web Scraping API</h1>
@@ -17,7 +20,49 @@ app.get("/", (req, res) => {
             </li>
         </ol>
     `);
-})
+});
+
+app.get("/space-marines", async (req, res) => {
+    var spaceMarineContent = {} //Create mutable JSON that will contain all faction information
+
+    await axios.get('https://wahapedia.ru/wh40k10ed/factions/space-marines/').then((response) => {
+        var html = response.data;
+        var $ = cheerio.load(html);
+
+        //Faction Title and Description
+        var factionTitle = $('.page_header_span').text();
+        var factionDescription = $('[id=wrapper] .Columns2 div:first p').text();
+
+        //Army Rules
+        var oathOfMoment = $('[id=wrapper] .Columns2 div:eq(0)').each(function(index, element){
+            var h3ToRemove = $('h3:first').remove();
+            var pToRemove = $('p:first').remove();
+        }).text();
+        
+
+        //Add page contents to JSON object
+        spaceMarineContent = {
+            "faction title" : factionTitle,
+            "faction description" : factionDescription,
+            "oath of moment" : oathOfMoment
+        }
+
+
+        /*Console logs*/
+        console.log("Title: " + factionTitle + "\n");
+        console.log("Description: " + factionDescription + "\n");
+        console.log("Oath Of Moment: " + oathOfMoment + "\n");
+        
+    })
+
+    res.json(spaceMarineContent);
+});
+
+app.get("/tyranids", (req, res) => {
+    res.send(`
+        TESTING TESTING TESTING
+    `);
+});
 
 app.listen(process.env.PORT || PORT, () => console.log(`server is running on PORT ${PORT}`));
 
