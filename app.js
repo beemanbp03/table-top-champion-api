bfc00335cd013f0507e71f1866341e67e8abecf7
beemanbp03 <boulder.payne.beeman@gmail.com>
2023-10-05 19:09:29 -0500

    added new units to the unit-datasheets.json file and edited app.js to read the unit-datasheets.json file and return an array of all units inside that file

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const PORT = 8000;
 const app = express();
+const fs = require('fs');
+
+const readJsonFile = (filePath) => {
+    try {
+        // Read the file
+        const jsonData = fs.readFileSync(filePath, 'utf8');
+        //Parse the JSON data into an array
+        const dataArray = JSON.parse(jsonData);
+
+        return dataArray;
+
+    } catch (err) {
+        console.log("Error reading JSON file: " + err);
+    }
+}
+
+const filePath = './json/space-marines/unit-datasheets.json';
+const dataArray = readJsonFile(filePath);
+console.log("unit-datasheets length: " + dataArray.length);
 
 app.get("/", (req, res) => {
     res.send(`<h1>API for Warhammer 40,000 10th Edition</h1>`);
