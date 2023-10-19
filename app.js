b0c9e6cc6d6cf4b39b136e3c253db71b488388ad
beemanbp03 <boulder.payne.beeman@gmail.com>
2023-10-19 11:14:11 -0500

    added endpoints for space-marines overview and specific units. Added endpoints for tyranids specific units

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ const readJsonFile = (filePath) => {
 }
 //Space Marines Array Length Output
 const spaceMarineFilePath = './json/space-marines/space-marine-unit-datasheets.json';
+const spaceMarineFactionFilePath = './json/space-marines/space-marine-faction-overview.json';
 const spaceMarineDataArray = readJsonFile(spaceMarineFilePath);
+const spaceMarineFactionOverview = readJsonFile(spaceMarineFactionFilePath);
 console.log("\nSpace Marines length: " + spaceMarineDataArray.length);
 
 //Tyranids Array Length Output
@@ -28,6 +30,28 @@ console.log("Tyranids length: " + tyranidsDataArray.length + "\n");
 
 app.get("/", (req, res) => {
     res.send(`<h1>API for Warhammer 40,000 10th Edition</h1>`);
+});
+
+//Space-Marine Endpoints
+app.get("/adeptus-astartes", (req, res) => {
+    res.send(`${JSON.stringify(spaceMarineFactionOverview, 0, 2)}`);
+});
+
+spaceMarineDataArray.forEach( (item, index) => {
+    app.get(`/adeptus-astartes/${item["uri-name"]}`, (req, res) => {
+        res.send(`${JSON.stringify(item, 0, 2)}`);
+    })
+});
+
+//Tyranid Endpoints
+app.get("/tyranids", (req, res) => {
+    res.send(`<h1>Faction Overview for Tyranids Coming Soon</h1>`);
+});
+
+tyranidsDataArray.forEach((item, index) => {
+    app.get(`/tyranids/${item["uri-name"]}`, (req, res) => {
+        res.send(JSON.stringify(item));
+    })
 });
 
 
